5a6a7704047412d58d8659ed1140ef6a31002a7d
MoreblessingMuskwe <more.hatendi@gmail.com>
2023-04-21 16:51:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print small case alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26 ; i++)
+	{
+		putchar(lc[i]);
+	}
+	putchar('\n');
+	(return (0))
+}
