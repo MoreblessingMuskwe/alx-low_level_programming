5fbac38bbd5c6f3f1d6cb1d5b6daf8d4bc13aac5
MoreblessingMuskwe <more.hatendi@gmail.com>
2023-04-25 15:29:25 -0700

    alphabet in lowecase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet in lowercase
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}	
